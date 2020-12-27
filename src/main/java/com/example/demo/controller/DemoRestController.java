8b84d871f1edb66c1576d2ca85dac8c16ea7d618
Pavel Ziryukin <zir-pav@mail.ru>
2020-12-27 16:31:24 +0300

    demo2 v2

--- src/main/java/com/example/demo/controller/DemoRestController.java
+++ src/main/java/com/example/demo/controller/DemoRestController.java
@@ -1,11 +1,7 @@
 package com.example.demo.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.AccessControlContext;
 
 @RestController
 public class DemoRestController {
